02ff5c694953f6931361bd1b08fa40f96212efc3
chengya <chengya@5kcrm.com>
2022-02-09 17:44:27 +0800

    编辑promise文件

--- ES6/Promise.js
+++ ES6/Promise.js
@@ -11,14 +11,25 @@
 // https://juejin.cn/post/6844903664516071438
 // 顺序执行异步任务
 // 使用 Promise.resolve()
+// <1>
 function mergePromise2(ajaxArray) {
   let p = Promise.resolve();
   let arr = [];
   ajaxArray.forEach(promise => {
     p = p.then(promise).then((data) => {
-        arr.push(data);
+        arr.push(data); // 此刻arr中每项都是执行完成的 `返回值`
         return arr;
     });
+  });
+  return p;
+}
+// <2>
+function mergePromise2(ajaxArray) {
+  let p = Promise.resolve();
+  let arr = [];
+  ajaxArray.forEach(promise => {
+    p = p.then(promise) // 会执行 promise
+    arr.push(p) // 此刻arr中每项都是执行完成的 `promise`
   });
   return p;
 }
