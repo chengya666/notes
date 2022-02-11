b7bcb3149716738e37312f1ef163881b371c9728
chengya <chengya@5kcrm.com>
2022-02-11 08:13:34 +0800

    补充promise

--- ES6/Promise.js
+++ ES6/Promise.js
@@ -9,14 +9,14 @@
 // 4.链式调用
 
 // https://juejin.cn/post/6844903664516071438
-// 顺序执行异步任务
+// <-- 顺序执行异步任务 -->
 // 使用 Promise.resolve()
 // <1>
 function mergePromise2(ajaxArray) {
   let p = Promise.resolve();
   let arr = [];
   ajaxArray.forEach(promise => {
-    p = p.then(promise).then((data) => {
+    p = p.then(promise).then((data) => { // 若promise为 `() => {}` 也会执行
         arr.push(data); // 此刻arr中每项都是执行完成的 `返回值`
         return arr;
     });
